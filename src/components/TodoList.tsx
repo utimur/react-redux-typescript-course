fed9b216afc59e35bd634a646e8a1238b5867b64
olejech <olejech@gmail.com>
2021-02-07 18:30:38 +0300

    Fix eslint warnings

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,40 +1,47 @@
-import React, {useEffect} from 'react';
-import {useTypedSelector} from "../hooks/useTypedSelector";
-import {useActions} from "../hooks/useActions";
+import React, { useEffect } from 'react'
+import { useTypedSelector } from '../hooks/useTypedSelector'
+import { useActions } from '../hooks/useActions'
 
 const TodoList: React.FC = () => {
-    const {page, error, loading, todos, limit} = useTypedSelector(state => state.todo)
-    const {fetchTodos, setTodoPage} = useActions()
-    const pages = [1, 2, 3, 4, 5]
+  const { page, error, loading, todos, limit } = useTypedSelector(
+    state => state.todo
+  )
+  const { fetchTodos, setTodoPage } = useActions()
+  const pages = [1, 2, 3, 4, 5]
 
-    useEffect(() => {
-        fetchTodos(page, limit)
-    }, [page])
+  useEffect(() => {
+    fetchTodos(page, limit)
+  }, [fetchTodos, limit, page])
 
-    if (loading) {
-        return <h1>Идет загрузка...</h1>
-    }
-    if (error) {
-        return <h1>{error}</h1>
-    }
+  if (loading) {
+    return <h1>Идет загрузка...</h1>
+  }
+  if (error) {
+    return <h1>{error}</h1>
+  }
 
-    return (
-        <div>
-            {todos.map(todo =>
-                <div key={todo.id}>{todo.id} - {todo.title}</div>
-            )}
-            <div style={{display: "flex"}}>
-                {pages.map(p =>
-                    <div
-                        onClick={() => setTodoPage(p)}
-                        style={{border:p === page ? '2px solid green' : '1px solid gray', padding: 10}}
-                    >
-                        {p}
-                    </div>
-                )}
-            </div>
+  return (
+    <div>
+      {todos.map(todo => (
+        <div key={todo.id}>
+          {todo.id} - {todo.title}
         </div>
-    );
-};
+      ))}
+      <div style={{ display: 'flex' }}>
+        {pages.map(p => (
+          <div
+            onClick={() => setTodoPage(p)}
+            style={{
+              border: p === page ? '2px solid green' : '1px solid gray',
+              padding: 10,
+            }}
+          >
+            {p}
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
 
-export default TodoList;
+export default TodoList
